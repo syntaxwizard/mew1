1cc2e4f861f0cd7a89561e07ece944ffab8e292e
syntaxwizard <135391534+syntaxwizard@users.noreply.github.com>
2024-01-07 15:56:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,80 @@
+const navBtn = document.querySelector('.nav-icon-btn');
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.header__top-row');
+
+navBtn.onclick = function (params) {
+	navIcon.classList.toggle('nav-icon--active');
+	nav.classList.toggle('header__top-row--mobile');
+	document.body.classList.toggle('no-scroll');
+};
+
+//phone mask//
+
+mask('[data-tel-input]');
+
+//нет '+' если ничего не введено//
+
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach(input => {
+	input.addEventListener('input', () => {
+		if (input.value == '+') input.value = '';
+	});
+
+	input.addEventListener('blur', () => {
+		if (input.value == '+') input.value = '';
+	});
+});
+
+//yandex-map//
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+	// Создание карты.
+	var map = new ymaps.Map('map', {
+		// Координаты центра карты.
+		// Порядок по умолчанию: «широта, долгота».
+		// Чтобы не определять координаты центра карты вручную,
+		// воспользуйтесь инструментом Определение координат.
+		center: [53.299875, 34.31146],
+		// Уровень масштабирования. Допустимые значения:
+		// от 0 (весь мир) до 19.
+		zoom: 18,
+	});
+
+	var myPlacemark = new ymaps.Placemark(
+		[53.299875, 34.31146],
+		{
+			balloonContent: `
+	<div class="balloon">
+
+		  <div class="balloon__address">hihihi</div>
+		   <div class="balloon__contacts">
+			  <a href="tel:+78121234567">+8 (812) 123-52-52</a>
+		   </div>
+	</div>
+		  `,
+		},
+		{
+			iconLayout: 'default#image',
+			iconImageHref: './img/map/location-pin.svg',
+			icon_imagesize: [40, 40],
+			iconImageOffset: [-39, 18],
+		}
+	);
+
+	map.controls.remove('geolocationControl'); // удаляем геолокацию
+	map.controls.remove('searchControl'); // удаляем поиск
+	map.controls.remove('trafficControl'); // удаляем контроль трафика
+	map.controls.remove('typeSelector'); // удаляем тип
+
+	// map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+	// map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+	map.controls.remove('rulerControl'); // удаляем контрол правил
+	map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+	map.geoObjects.add(myPlacemark);
+
+	//myPlacemark.balloon.open();//
+}
